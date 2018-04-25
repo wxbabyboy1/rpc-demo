faa2e9f9d73980bced0837129718e1d1e3595fcd
jinweile <jinweile@163.com>
2018-04-25 23:22:20 +0800

    socket http demo commit

--- socket-http.demo/src/main/java/socket/http/demo/TestDemo.java
+++ socket-http.demo/src/main/java/socket/http/demo/TestDemo.java
@@ -7,13 +7,14 @@ import java.io.IOException;
 public class TestDemo {
 
     public static void main(String[] args) throws IOException {
-        String url = "http://www.mtime.com";
+        String url = "http://www.epson.jp";
         String content = "";
         try{
             content = new SimpleOneceAgent().ReadUrlContent(url);
         }catch(Exception ex){
             ex.printStackTrace();
         }
+        System.out.println(content);
         System.in.read();
     }
 
